c7cf0bd938582c5791829700c40910e3eb004508
seojuny95 <sytwoyou@gmail.com>
2021-05-26 17:12:14 +0900

    leetcode: 3 problems solved

---
+++ seojun/range-sum-BST.js
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var rangeSumBST = function (root, low, high) {
+  let sum = 0;
+
+  const traverse = node => {
+    if (!node) return;
+
+    if (node.val >= low && node.val <= high) {
+      sum += node.val;
+    }
+
+    traverse(node.left);
+    traverse(node.right);
+  };
+
+  traverse(root);
+
+  return sum;
+};
+
+
+// const result = [];
+
+// let currentNode = root;
+
+// while(currentNode) {
+//     if (currentNode.val <= low && currentNode.val >= high) {
+//         result.push(currentNode.val);
+//     }
+
+//     if (currentNode.val > low) {
+//         currentNode = currentNode.left;
+//     } else if (currentNode.val < high) {
+//         currentNode = currentNode.right;
+//     }
+// }
+
+// return result.reduce((a, b) => a + b);
